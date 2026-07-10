0f94f59b508eaca3570307612d8e4bbd95b8eac3
rajneeshbhaskar504-gif <rajneeshbhaskar504@gmail.com>
2026-07-10 12:00:05 +0530

    Update video.py

--- video.py
+++ video.py
@@ -1,18 +1,1 @@
-from moviepy import ImageClip, AudioFileClip, concatenate_videoclips
 
-def create_video(images, audio_file, output="final_video.mp4"):
-    audio = AudioFileClip(audio_file)
-
-    duration = audio.duration / len(images)
-
-    clips = []
-    for img in images:
-        clip = ImageClip(img).with_duration(duration)
-        clips.append(clip)
-
-    video = concatenate_videoclips(clips)
-    video = video.with_audio(audio)
-
-    video.write_videofile(output, fps=24)
-
-    return output
