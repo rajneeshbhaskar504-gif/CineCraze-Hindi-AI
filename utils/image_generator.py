61917151adeb953b0ece33575a52e6d3771b6db9
rajneeshbhaskar504-gif <rajneeshbhaskar504@gmail.com>
2026-07-10 12:09:35 +0530

    Update image_generator.py

--- utils/image_generator.py
+++ utils/image_generator.py
@@ -1,22 +1,29 @@
 import google.generativeai as genai
 
 def generate_image_prompt(scene):
+    try:
+        model = genai.GenerativeModel("gemini-1.5-flash")
 
-    model = genai.GenerativeModel("gemini-2.5-flash")
-
-    prompt = f"""
-Create one detailed animation image prompt.
+        prompt = f"""
+Create ONE detailed animation image prompt.
 
 Style:
-Original 3D animated movie, cinematic lighting, expressive characters, ultra detailed.
+Original 3D animated movie,
+Pixar-inspired,
+cinematic lighting,
+expressive characters,
+ultra detailed,
+8K quality.
 
 Scene:
-
 {scene}
 
-Return only the image prompt.
+Return ONLY the image prompt.
 """
 
-    response = model.generate_content(prompt)
+        response = model.generate_content(prompt)
 
-    return response.text
+        return response.text
+
+    except Exception as e:
+        return f"Error: {e}"
