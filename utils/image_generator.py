a51b3fa0e99fcfc47930d849daa8cb91b37342cb
rajneeshbhaskar504-gif <rajneeshbhaskar504@gmail.com>
2026-07-10 12:28:06 +0530

    Update image_generator.py

--- utils/image_generator.py
+++ utils/image_generator.py
@@ -1,22 +1,28 @@
 import requests
 import os
 
-API_URL = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell"
+API_URL = "https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-schnell"
 
 def generate_image(prompt, output_file, hf_token):
+
     headers = {
-        "Authorization": f"Bearer {hf_token}"
+        "Authorization": f"Bearer {hf_token}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "inputs": prompt
     }
 
     response = requests.post(
         API_URL,
         headers=headers,
-        json={"inputs": prompt},
+        json=payload,
         timeout=300
     )
 
     if response.status_code != 200:
-        raise Exception(f"Hugging Face Error: {response.text}")
+        raise Exception(response.text)
 
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
