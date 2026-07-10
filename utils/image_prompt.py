15260720a657d2c7235ebe512f0b5147c0d54c07
rajneeshbhaskar504-gif <rajneeshbhaskar504@gmail.com>
2026-07-10 12:05:55 +0530

    Update image_prompt.py

--- utils/image_prompt.py
+++ utils/image_prompt.py
@@ -1,10 +1,10 @@
 import google.generativeai as genai
 
 def generate_scene_prompts(movie_name, script):
+    try:
+        model = genai.GenerativeModel("gemini-1.5-flash")
 
-    model = genai.GenerativeModel("gemini-2.5-flash")
-
-    prompt = f"""
+        prompt = f"""
 You are an AI Animation Director.
 
 Movie: {movie_name}
@@ -16,20 +16,20 @@ Convert this script into 20 animation scenes.
 
 For each scene generate:
 
-Scene Number
-
-Scene Description
+1. Scene Number
+2. Scene Description
+3. Camera Angle
+4. Image Prompt
 
 Animation Style:
-Pixar inspired 3D, cinematic lighting, expressive characters, colorful, ultra detailed, original design
-
-Camera Angle
-
-Image Prompt
+Pixar inspired 3D animation, cinematic lighting, expressive characters, colorful, ultra detailed, original design.
 
 Return only scene prompts.
 """
 
-    response = model.generate_content(prompt)
+        response = model.generate_content(prompt)
 
-    return response.text
+        return response.text
+
+    except Exception as e:
+        return f"Error: {e}"
